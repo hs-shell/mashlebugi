9b89428bb8dee38d47e7a562facfc2195a48dd9d
6-keem <6ukeem@gmail.com>
2025-02-01 21:17:48 +0900

    feat: update icons

--- vite.config.ts
+++ vite.config.ts
@@ -6,31 +6,33 @@ import { defineConfig } from 'vite';
 
 import manifest from './manifest.config';
 
-export default defineConfig({
-  base: './',
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    /**
-     * npm run dev 시 주석 처리 해주세요
-     */
-    crx({
-      manifest,
-      contentScripts: {
-        injectCss: false,
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        popup: 'index.html',
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development';
+  return {
+    base: './',
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      // dev 환경에서는 crx 플러그인을 제외
+      !isDev &&
+        crx({
+          manifest,
+          contentScripts: {
+            injectCss: true,
+          },
+        }),
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
     },
-  },
+    build: {
+      rollupOptions: {
+        input: {
+          popup: 'index.html',
+        },
+      },
+    },
+  };
 });
